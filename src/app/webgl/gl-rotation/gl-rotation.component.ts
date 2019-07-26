ce1d827dfe6de69a1a9b84a7b76deb0e271ad9e0
lidm <lidm@cloudream.com>
2019-07-26 17:18:02 +0800

    feat: 🎸 新增旋转功能
    1. 公式 x = px * ry + py * rx ; y = py * ry - px * rx;

--- src/app/webgl/gl-rotation/gl-rotation.component.ts
+++ src/app/webgl/gl-rotation/gl-rotation.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {createProgramFromText} from '../../utils/gl-utils';
+import {createProgramFromText, resizeCanvasToDisplaySize} from '../../utils/gl-utils';
 // @ts-ignore
 import vertSource from './vert.vert';
 // @ts-ignore
@@ -13,18 +13,73 @@ import fragSource from './frag.frag';
 export class GlRotationComponent implements OnInit {
   constructor() {
   }
-
+  program?: WebGLProgram = undefined;
+  gl?: WebGLRenderingContext = undefined
+  angle = 0
   ngOnInit() {
     this.onDraw();
   }
 
-  onDraw() {
+  onDraw = () => {
     const canvas = document.querySelector('#c');
     // @ts-ignore
-    const gl = canvas.getContext('webgl');
+    const gl = this.gl = canvas.getContext('webgl');
+    console.log(gl)
     if (!gl) {
       return new Error('no webgl');
     }
-    const propame = createProgramFromText(gl, '', '');
+    const program = this.program = createProgramFromText(gl, vertSource, fragSource);
+    const positionsAttrLocation = gl.getAttribLocation(program, 'a_position');
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.useProgram(program)
+    const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+    const colorUniformLocation = gl.getUniformLocation(program, 'u_color');
+    gl.uniform4f(colorUniformLocation, 100, 100, 100, 1);
+    const trasitonUniformLocation = gl.getUniformLocation(program, 'u_trasiton')
+    gl.uniform2f(trasitonUniformLocation, 200, 200)
+    const position = [
+      // 1
+      0, 0,
+      20, 0,
+      0, 100,
+      0, 100,
+      20, 0,
+      20, 100,
+      // 2
+      20, 0,
+      100, 0,
+      20, 20,
+      20, 20,
+      100, 20,
+      100, 0,
+      // 3
+      20, 30,
+      100, 30,
+      20, 50,
+      20, 50,
+      100, 50,
+      100, 30
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(position), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.vertexAttribPointer(gl, 2,  gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(positionsAttrLocation);
+    resizeCanvasToDisplaySize(gl.canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    this.onEnterDraw();
+  }
+  onEnterDraw = () => {
+    const gl = this.gl
+    const angle = 360 - this.angle
+    this.angle += 0.01
+    const rotation = [Math.sin(angle), Math.cos(angle)]
+    const rotationUniformLocation = gl.getUniformLocation(this.program, 'u_rotation');
+    gl.uniform2fv(rotationUniformLocation, rotation);
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.clearColor(0, 0, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, 6 * 3);
+    requestAnimationFrame(this.onEnterDraw);
   }
 }
--- src/app/webgl/gl-rotation/vert.vert
+++ src/app/webgl/gl-rotation/vert.vert
@@ -3,7 +3,11 @@ uniform vec2 u_resolution;
 uniform vec2 u_trasiton;
 uniform vec2 u_rotation;
 void main() {
-  vec2 zeroToOne = a_position / u_resolution;
+  vec2 rotationPosition = vec2(
+  a_position.x * u_rotation.y + a_position.y * u_rotation.x,
+  a_position.y * u_rotation.y - a_position.x * u_rotation.x
+  );
+  vec2 zeroToOne = rotationPosition / u_resolution;
   vec2 oneToTwo = zeroToOne * 2.0;
   vec2 trasition = u_trasiton / u_resolution;
   vec2 clipSpace = oneToTwo - 1.0;
